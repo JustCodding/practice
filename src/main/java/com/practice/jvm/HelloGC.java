181c76ec7eb6e1e2a42ee59867d03f0e8dea61e0
cjpfor <cjpfor@163.com>
2019-08-05 17:28:03 +0800

    add

--- src/main/java/com/practice/jvm/HelloGC.java
+++ src/main/java/com/practice/jvm/HelloGC.java
@@ -7,10 +7,6 @@ public class HelloGC extends OutClass{
     public static void main(String[] args) {
 
         System.out.println("**************hello GC*****************");
-        try {
-            Thread.sleep(Integer.MAX_VALUE);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        byte[] bytes = new byte[20*1024*1024];
     }
 }
